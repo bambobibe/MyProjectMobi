d98b4acef600132f047c1e4f11d9a3eb9133b138
frontend <67629946+bambobibe@users.noreply.github.com>
2020-07-15 14:05:25 +0700

    add Slide bootstrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React , { Component } from 'react';
+import Slide from "./Slide"
 
 class Header extends Component{
     render(){
@@ -83,42 +84,7 @@ class Header extends Component{
 					{/* </nav> */}
 					</div>
 				</div>
-				<div style={{"padding-top":"130px"}}>
-					<div className="container">
-					<div id="myCarousel" className="carousel slide" data-ride="carousel">
-						<ul className="carousel-indicators">
-						<li data-target="#myCarousel" data-slide-to={0} className="active" />
-						<li data-target="#myCarousel" data-slide-to={1} />
-						<li data-target="#myCarousel" data-slide-to={2} />
-						<li data-target="#myCarousel" data-slide-to={3} />
-						<li data-target="#myCarousel" data-slide-to={4} />
-						</ul>
-						<div className="carousel-inner">
-						<div className="carousel-item active">
-							<img src="./img/banners/banner0.gif" width={1120} height={250} />
-						</div>
-						<div className="carousel-item">
-							<img src="./img/banners/banner1.png" width={1120} height={250} />
-						</div>
-						<div className="carousel-item">
-							<img src="./img/banners/banner2.png" width={1120} height={250} />
-						</div>
-						<div className="carousel-item">
-							<img src="./img/banners/banner3.png" width={1120} height={250} />
-						</div>
-						<div className="carousel-item">
-							<img src="./img/banners/banner4.png" width={1120} height={250} />
-						</div>
-						</div>
-						<a className="carousel-control-prev" href="#myCarousel" data-slide="prev">
-						<span className="carousel-control-prev-icon" />
-						</a>
-						<a className="carousel-control-next" href="#myCarousel" data-slide="next">
-						<span className="carousel-control-next-icon" />
-						</a>
-					</div>
-				</div>
-			</div>
+				<Slide />
 		</div>
         )  
 	}
